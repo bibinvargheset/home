3c7a9ea0faff1cb8c591afbf0e51412b451a77dc
Bibin Varghese <bibinvargheset@gmail.com>
2020-08-26 14:04:36 +0530

    Updates

---
+++ precache-manifest.b3aad8cd9d3f07e9bb8a32f0733f83de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dae457372bf3de14511e58b6e77cfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e99d98e607be8618c1a1",
+    "url": "/static/css/2.6edc7bb3.chunk.css"
+  },
+  {
+    "revision": "d274ad5b7a07669bb6fd",
+    "url": "/static/css/main.669ac10a.chunk.css"
+  },
+  {
+    "revision": "e99d98e607be8618c1a1",
+    "url": "/static/js/2.ea0ad1fd.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ea0ad1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d274ad5b7a07669bb6fd",
+    "url": "/static/js/main.2c0f8c8f.chunk.js"
+  },
+  {
+    "revision": "83b610bb7fd53e839a46",
+    "url": "/static/js/runtime-main.9f1dc9a2.js"
+  },
+  {
+    "revision": "cb04491815ef5b704601e4394b782e9d",
+    "url": "/static/media/resume.cb044918.pdf"
+  }
+]);
